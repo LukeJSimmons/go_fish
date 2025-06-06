7551f215566f9e4f390a26d6f3f87e85f000f8f4
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-06 15:37:33 -0400

    Add accept_new_client

--- lib/fish_socket_server.rb
+++ lib/fish_socket_server.rb
@@ -2,6 +2,10 @@ class FishSocketServer
   def port_number
     3000
   end
+
+  def clients
+    @clients ||= []
+  end
   
   def start
     @server = TCPServer.new(port_number)
@@ -9,5 +13,10 @@ class FishSocketServer
 
   def stop
     @server.close if @server
+  end
+
+  def accept_new_client(player_name="Random Player")
+    client = @server.accept_nonblock
+    clients << client
   end
 end
